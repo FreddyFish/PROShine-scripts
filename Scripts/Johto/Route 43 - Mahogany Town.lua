f5190dc9b575595156c5c968a8e2a41a7eef010d
Pepsai <Pepsai@live.dk>
2016-09-11 17:34:13 +0200

    Added route 43

---
+++ Scripts/Johto/Route 43 - Mahogany Town.lua
@@ -0,0 +1,43 @@
+-- Copyright © 2016 FreddyFish
+-- This work is free. You can redistribute it and/or modify it under the
+-- terms of the Do What The Fuck You Want To Public License, Version 2,
+-- as published by Sam Hocevar. See the LICENSE file for more details.
+
+name = "Leveling: Route 43 (Near Mahogany Town)"
+author = "FreddyFish - Thanks to Silv3r for the template"
+description = [[This script will train the first pokémon of your team.
+It will also try to capture shinies by throwing pokéballs.
+Start anywhere between Mahogany Town and Route 43.]]
+
+function onPathAction()
+	if isPokemonUsable(1) then
+		if getMapName() == "Pokecenter Mahogany" then
+			moveToMap("Mahogany Town")
+		elseif getMapName() == "Mahogany Town" then
+			moveToMap("Route 43")
+		elseif getMapName() == "Route 43" then
+			moveToGrass()
+		end
+	else
+		if getMapName() == "Route 43" then
+			moveToMap("Mahogany Town")
+		elseif getMapName() == "Mahogany Town" then
+			moveToMap("Pokecenter Mahogany")
+		elseif getMapName() == "Pokecenter Mahogany"	 then
+			usePokecenter()
+		end
+	end
+end
+
+function onBattleAction()
+	if isWildBattle() and isOpponentShiny() then
+		if useItem("Ultra Ball") or useItem("Great Ball") or useItem("Pokeball") then
+			return
+		end
+	end
+	if getActivePokemonNumber() == 1 then
+		return attack() or sendUsablePokemon() or run() or sendAnyPokemon()
+	else
+		return run() or attack() or sendUsablePokemon() or sendAnyPokemon()
+	end
+end
